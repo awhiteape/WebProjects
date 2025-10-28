c723cbb861b55a288db61f74d941c2aaf46e8f8e
Jacob Lindholm <lindholmjacob@gmail.com>
2025-10-27 22:33:14 -0700

    Added button event listeners and basic functions

--- extensions/stockCalculator/index.js
+++ extensions/stockCalculator/index.js
@@ -1,8 +1,22 @@
 const roa = document.getElementById('roa-form');
 const roaButton = document.getElementById('roa-submit');
+const peForm = document.getElementById('pe-form');
+const peButton = document.getElementById('pe-submit');
+const stockForm = document.getElementById('stock-form');
+const stockButton = document.getElementById('save-stock');
 
 roaButton.addEventListener('click', (event) => {
     event.preventDefault();
     const roaResult = document.getElementById('roa-result');
     roaResult.textContent = 'Result: Clicked';
+})
+
+peButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log("pe calculated");
+})
+
+stockButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log("Stock added!");
 })
